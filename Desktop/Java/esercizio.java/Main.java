b6a2afcad39c48db314b8ee71398e9d7f670f358
Jamming <j.lucreziaparrillo@gmail.com>
2024-01-17 13:57:23 +0100

    added files to repository esercizio1

---
+++ Desktop/Java/esercizio.java/Main.java
@@ -0,0 +1,12 @@
+
+      class Main {
+        
+        public static void main(String[] args) {
+          Studente studentData = new Studente("Anna","Maggio","AZ94R6");
+          System.out.println("Students data: "+ studentData.firstName + ", "+ studentData.lastName + ", esercizio.javaù" + studentData.ID);
+        }
+ };       
+  
+  
+
+        
---
+++ Desktop/Java/esercizio.java/Studente.java
@@ -0,0 +1,24 @@
+public class Studente {
+    String firstName;
+    String lastName;
+    String ID;
+
+    public Studente(String firstName, String lastName, String ID){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ID = ID;
+    };
+    
+}
+
+
+
+
+
+
+
+
+//  Scrivere un programma che contenga una classe che definisce uno Studente che abbia come proprietà nome, 
+//cognome ed identificativo.
+//  Per questa classe saranno necessari i metodi accessori (getters e setters) ed un costruttore.
+// Nel main dichiarare un oggetto di tipo Studente.
