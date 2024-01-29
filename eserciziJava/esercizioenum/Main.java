61aab7bcb2e14d5b48b4b1a43d4f6d147ff9549b
Jamming <j.lucreziaparrillo@gmail.com>
2024-01-29 19:07:59 +0100

    "esercizio enum update"
    Signed-off-by: Jamming <j.lucreziaparrillo@gmail.com>

--- eserciziJava/esercizioenum/Main.java
+++ eserciziJava/esercizioenum/Main.java
@@ -1,17 +1,96 @@
 package eserciziJava.esercizioenum;
 
-import eserciziJava.esercizioenum.Rettangolo;
-import eserciziJava.esercizioenum.Triangolo;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args){
+        RettangoloEnum rettangoloEnum = new RettangoloEnum(9.0,8.0);
+        Triangolo triangolo = new Triangolo(9.0,8.0);
+        System.out.println("l'area è = "+ rettangoloEnum.calcolaArea() +"tipo: "+ rettangoloEnum.getTipo());
+        System.out.println("l'area è = "+ triangolo.calcolaArea() +"tipo: "+ triangolo.getTipo());
+    }
 
+}
 
-        Triangolo triangolo = new Triangolo(8, 8,TipoForma.TRIANGOLO);
-        Rettangolo rettangolo = new Rettangolo(8, 8,TipoForma.RETTANGOLO);
+enum TipoForma{
+TRIANGOLO,
+    RETTANGOLO
+}
 
-        System.out.println("l' area del triangolo è = " + triangolo.calcolaArea());
-        System.out.println("l'area del rettangolo è = " + rettangolo.calcolaArea());
+class Forma{
+    private TipoForma tipo;
 
+    public TipoForma getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoForma tipo) {
+        this.tipo = tipo;
+    }
+    public double calcolaArea(){
+        System.out.println("l'area della forma è = ");
+        return 0.0;
+    }
+}
+
+class RettangoloEnum extends Forma{
+    double base = 0.0;
+    double altezza = 0.0;
+
+    public RettangoloEnum(double base,double altezza){
+        this.base = base;
+        this.altezza= altezza;
+        super.setTipo(TipoForma.RETTANGOLO);
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaArea() {
+        return base*altezza;
+    }
+}
+
+class Triangolo extends Forma{
+    double base = 0.0;
+    double altezza = 0.0;
+
+    public Triangolo(double base,double altezza){
+        this.base = base;
+        this.altezza= altezza;
+        super.setTipo(TipoForma.TRIANGOLO);
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaArea() {
+        return (base*altezza)/2;
     }
 }
